562152cad470ba6f5a2ab22710203e8876e442b2
Evgeniia (Zhenya) Dolgikh <104003951+zhenyadolgikh@users.noreply.github.com>
2024-10-12 17:52:25 +0200

    main menu added

---
+++ Assets/Scripts/MenuCameraRotate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraRotate : MonoBehaviour
+{
+    public float rotationSpeed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
